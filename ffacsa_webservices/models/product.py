9a09faeda19464821cf280f73a5bcc83aa913e2c
Abraham Febres <afebres@xetechs.com>
2021-06-02 11:37:52 -0400

    refix: add service_to_purchase

--- ffacsa_webservices/models/product.py
+++ ffacsa_webservices/models/product.py
@@ -7,3 +7,5 @@ class ProductTemplate(models.Model):
     _description = 'Product Template'
     
     code = fields.Char(string='Code')
+    service_to_purchase = fields.Boolean("Purchase Automatically", help="If ticked, each time you sell this product through a SO, a RfQ is automatically created to buy the product." 
+                "Tip: don't forget to set a vendor on the product.")
